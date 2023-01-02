ff8fc8f560eaba2aac5c895cd511c46f7695f883
disdong123 <gksrlfw@gmail.com>
2023-01-03 00:41:09 +0900

    workflow 수정 - ecr -> dockerhub 로 변경

--- subprojects/server/src/main/kotlin/kr/disdong/gradle/multi/module/template/server/controller/TestController.kt
+++ subprojects/server/src/main/kotlin/kr/disdong/gradle/multi/module/template/server/controller/TestController.kt
@@ -1,6 +1,7 @@
 package kr.disdong.gradle.multi.module.template.server.controller
 
 import kr.disdong.gradle.multi.module.template.core.mysql.repository.PostRepository
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,9 +10,14 @@ class TestController(
     private val postRepository: PostRepository
 ) {
 
-    @GetMapping("/t1")
-    fun a() {
+    val logger = LoggerFactory.getLogger(TestController::class.java)
+
+    @GetMapping("/health")
+    fun health(): String {
+        logger.info("1 ${postRepository.findAll()[0].id}")
+        logger.info("2 ${postRepository.hello()[0].id}")
         println(postRepository.findAll()[0].id)
         println(postRepository.hello()[0].id)
+        return "knitting server! 워니쨩"
     }
 }
