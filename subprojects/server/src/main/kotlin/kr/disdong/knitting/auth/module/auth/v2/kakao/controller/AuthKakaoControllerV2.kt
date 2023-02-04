360ec5c88539975ce76413593bcf521173f10bb0
disdong123 <gksrlfw@gmail.com>
2023-02-04 15:26:23 +0900

    response 값 변경

--- subprojects/server/src/main/kotlin/kr/disdong/knitting/auth/module/auth/v2/kakao/controller/AuthKakaoControllerV2.kt
+++ subprojects/server/src/main/kotlin/kr/disdong/knitting/auth/module/auth/v2/kakao/controller/AuthKakaoControllerV2.kt
@@ -4,6 +4,7 @@ import kr.disdong.knitting.auth.kakao.v2.KakaoServiceV2
 import kr.disdong.knitting.auth.kakao.v2.dto.OAuthCallbackResponseV2
 import kr.disdong.knitting.auth.module.auth.v2.kakao.dto.LoginResponseV2
 import kr.disdong.knitting.auth.module.auth.v2.kakao.service.AuthKakaoServiceV2
+import kr.disdong.knitting.common.dto.KnittingResponse
 import kr.disdong.knitting.common.logger.logger
 import org.springframework.web.bind.annotation.*
 
@@ -21,10 +22,10 @@ class AuthKakaoControllerV2(
      * @param response
      */
     @GetMapping("/callback")
-    fun login(response: OAuthCallbackResponseV2): LoginResponseV2 {
+    fun login(response: OAuthCallbackResponseV2): KnittingResponse<LoginResponseV2> {
         logger.info("login(response: $response)")
 
-        return authKakaoServiceV2.login(response)
+        return KnittingResponse.of(authKakaoServiceV2.login(response))
     }
 
     /**
