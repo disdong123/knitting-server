daa42f333d660541f9f770b892c895b72c600f55
disdong123 <gksrlfw@gmail.com>
2023-02-11 12:01:49 +0900

    kr.disdong:spring-version-catalog 생성 및 적용

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,26 @@ include(
 enableFeaturePreview("VERSION_CATALOGS")
 
 dependencyResolutionManagement {
+    fun ExtraPropertiesExtension.findProperty(name: String): String? {
+        return if (has(name)) {
+            get(name) as String?
+        } else {
+            null
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/disdong123/version-catalog")
+            credentials {
+                username = extra.findProperty("gpr.user") ?: System.getenv("DISDONG_USERNAME")
+                password = extra.findProperty("gpr.key") ?: System.getenv("DISDONG_TOKEN")
+            }
+        }
+    }
     versionCatalogs {
         create("libs") {
-            from(files("libs.version.toml"))
+            from("kr.disdong:spring-version-catalog:0.0.2")
         }
     }
 }
